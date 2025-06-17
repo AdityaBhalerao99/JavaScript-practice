ffed6a96996b8000d0b52eea88e307b698fe0c2f
AdityaBhalerao99 <adibhalerao1999@gmail.com>
2025-06-17 16:31:59 +0530

    conversionOperations

---
+++ Basics/03_conversionOperaton.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+//console.log(typeof score);
+let num = Number(score)
+//console.log(num);
+//console.log(typeof num);
+
+//conversions in java
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1 ; false => 0
+
+let isLoggedIn = "aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+/*
+any data type to Boolean 
+1 => true; 0 => false
+"" => false
+"hitesh" => true
+*/
+
+let someNumber = 223
+
+let convertedString = String(someNumber)
+console.log(convertedString)
+console.log(typeof convertedString)
+
+

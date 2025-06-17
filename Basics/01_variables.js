dfeb9529eb76a95deb4f635b68473e0e84aeb55e
AdityaBhalerao99 <adibhalerao1999@gmail.com>
2025-06-17 14:43:58 +0530

    data types

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,1 +1,16 @@
-console.log("aditya")
+const accountId = 12345 //const is keyword and oncce initialized with value , we cannot change
+let accountEmail = "adityabhalerao@gmail.com"
+let accountPassword  = "12435"
+var accountName = "Aditya" // var is a keyword
+let accountState; //if printed this will show 'undefined'
+
+/*
+prefer not to use 'var'
+because of issue in block scope and functional scpoe
+*/
+
+accountCity = "Pune" // this will automatically detect the variable
+
+console.log(accountId,accountEmail,accountPassword,accountName,accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountName,accountCity,accountState]) //console.table([]) is used to print the values in table format
---
+++ Basics/02_daataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treats all JS code as newer version
+
+//alert(3 + 3) // we are usnig nodeJs and not browser
+
+console.log(8+8)
+
+let name = "aditya"
+let age = 26
+let isLoggedIn = true
+let state;              
+let contact = null
+
+// Primitive Data Types : 
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null - standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof contact)  // object
+console.log(typeof state)  // undefined

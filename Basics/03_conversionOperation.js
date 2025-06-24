43f7aa22f762b0ad879404ad70b387b19379467d
AdityaBhalerao99 <adibhalerao1999@gmail.com>
2025-06-24 11:41:04 +0530

    Operations

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+// let score = "33"
+
+// //console.log(typeof score);
+// let num = Number(score)
+// //console.log(num);
+// //console.log(typeof num);
+
+// //conversions in java
+// // "33" => 33
+// // "33abc" => NaN(Not a Number)
+// // true => 1 ; false => 0
+
+// let isLoggedIn = "aditya"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// /*
+// any data type to Boolean 
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+// */
+
+// let someNumber = 223
+
+// let convertedString = String(someNumber)
+// console.log(convertedString)
+// console.log(typeof convertedString)
+
+
+//***************Operations****************
+
+// let value = 3
+// let negValue = -value
+// //console.log(negValue)
+
+// console.log("addition : "+2+2) // addition
+// console.log("Substraction : "+2-2) //Substraction
+// /*
+// Concatenation first:
+// "Substraction : " + 2
+// → This becomes the string: "Substraction : 2"
+
+// Then subtraction:
+// "Substraction : 2" - 2
+// → JavaScript tries to subtract a number (2) from a string ("Substraction : 2"), which cannot be converted to a number.
+// so the output will be NaN(Not a Number)
+// To avoid this write 2-2 in a bracket like : console.log("Substraction : "+(2-2))
+// */
+// console.log("Multiplication : "+2*2) //Multiplication 
+// console.log("Power i.e 2 power 3 OR 2 raised to 3 : "+2**3) //Power i.e 2 power 3 OR 2 raised to 3
+// console.log("Division : "+2/2)  //Division
+// console.log("Remainder i.e Mod Operation of 2%2 : "+2%2)  //Remainder i.e Mod Operation
+
+
+
+//********************Concatination Example********************
+
+//Normal concatenation
+let fname = "Aditya"
+let lname = " Bhalerao"
+let Name = fname + lname;
+console.log(Name) 
